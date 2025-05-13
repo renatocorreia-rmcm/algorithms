9bb42838262d171cfe9ec37a194c74ff457e898f
rmcm <rmcm@g2c14.cin.ufpe.br>
2025-05-13 15:08:15 -0300

    slide implementation

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -5,9 +5,9 @@ using namespace std;
 void merge(int* a, int l, int r){
     
     int temp[r-l+1];
-    for (int i = l; i < r; i++)
+    for (int i = l; i <= r; i++)
     {
-        temp[i] = a[i];
+        temp[i-l] = a[i];
     }
 
     int m = (l+r)/2;
@@ -15,27 +15,27 @@ void merge(int* a, int l, int r){
     int i1=l;
     int i2=m+1;   
     
-    for (int curr = l; curr < r; curr++)
+    for (int curr = l; curr <= r; curr++)
     {
         // ended some block
         if (i1==m+1)
         {
-            a[curr] = temp[i2++];
+            a[curr] = temp[i2++-l];
         }
         else if (i2>r)
         {
-            a[curr] = temp[i1++];
+            a[curr] = temp[i1++-l];
         }
         // both blocks not ended
         else
         {
-            if (temp[i1]<temp[i2])
+            if (temp[i1-l]<temp[i2-l])
             {
-                a[curr] = temp[i1++];
+                a[curr] = temp[i1++-l];
             }
             else
             {
-                a[curr] = temp[i2++];
+                a[curr] = temp[i2++-l];
             }
            
         }
@@ -45,10 +45,15 @@ void merge(int* a, int l, int r){
 }
 
 void mergesort(int* a, int l, int r){
+    if (l<r)
+    {
+    
     int m = (l+r)/2;
     mergesort(a,l,m);
     mergesort(a,m+1,r);
     merge(a,l,r);
+    }
+
 }
 
 
@@ -57,11 +62,11 @@ void mergesort(int* a, int l, int r){
 
 int main(){
 
-    int arr[] = {1, 9, 2, 8, 3, 7, 4, 6, 5, 0};
+    int arr[] = {5,2,1,7,0};
 
-    mergesort(arr, 0, 9);
+    mergesort(arr, 0, 4);
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         cout << arr[i] << " ";
     }
