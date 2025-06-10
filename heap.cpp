593be5fa9dcb9842ba9aba96f8b857ebea278246
renatocorreia12144-rmcm <rmcm@cin.ufpe.br>
2025-06-10 18:28:24 -0300

    bottom up

---
+++ heap.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+/*
+left    2*i
+right   2*i+1
+parent  i//2
+*/
+
+class heap
+{
+public:
+    int* arr;
+    int n;
+
+
+    void heap_bottom_up(){
+        int i_lastparent = n/2;  // tree last parent (rightmost node not leaf)
+        for (int i = i_lastparent; i > 1; i--)  // iterate from last parent to root
+        {
+            int k = i;
+            int v = arr[k];
+
+            bool heap = false;
+
+            int i_leftchild = 2*k;
+            int i_rightchild = 2*k+1;
+
+            
+            while (!heap && i_leftchild <=n)
+            {
+                
+                /*   SET BIGGER CHILD   */
+                int i_biggerchild = i_leftchild;  // default bigger since right child may not exist
+                if (i_biggerchild < n)  // range checking to see if right child exists
+                {
+                    if (arr[i_rightchild] > arr[i_leftchild])
+                    {
+                        i_biggerchild = i_rightchild;
+                    }
+                }
+                /*  COMPARE TO PARENT   */
+                if (v>=arr[i_biggerchild])
+                {
+                    heap = true;
+                }
+                else
+                {
+                    arr[k] = arr[i_biggerchild];
+                    k = i_biggerchild;
+                }
+
+                arr[k] = v;
+                i_leftchild  = 2*k;
+                i_rightchild = 2*k+1;
+                
+            }
+            
+
+
+        }
+        
+    }
+
+    heap(int* arr, int n){
+
+        this->arr = arr;
+        this->n=n;
+        heap_bottom_up();
+
+    }
+};
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
