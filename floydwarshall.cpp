855d3752ba304619b6b8049cf44c6012c624800f
rmcm <rmcm@g1c24.cin.ufpe.br>
2025-07-07 15:49:26 -0300

    finished floydwarshall

--- floydwarshall.cpp
+++ floydwarshall.cpp
@@ -1,56 +1,61 @@
 #include <iostream>
+#include <climits>
+using namespace std;
 
 
-inline int min(int a, int b) {return b<a?b:a;}
+#define INFINITY INT_MAX / 2
+const int V = 4;
 
 
-/*
-consider "dp" a 3d matrix to act as a memory
-n (n x n)
-a list of matrix where each matrix t=corresponds to a state
+void floydwarshall(int graph[V][V]) {
+    int dp[V + 1][V][V];
 
-so dp[k][i][j] tells the minimun distance going from i to j considering only the nodes from 0 to break
-so dp[n-1 is the solution]
-*/
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dp[0][i][j] = graph[i][j];
 
+    for (int k = 1; k <= V; k++)
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++) {
 
-void floydwarshall(int* graph[], int V) {  // graph matrix: weights between node i to j without considering transitivity - considering only adjancencys
+                if (
+                    dp[k-1][i][k-1] < INFINITY 
+                    && dp[k-1][k-1][j] < INFINITY
+                    )
+                {
+                    dp[k][i][j] = min(dp[k-1][i][j], dp[k-1][i][k-1] + dp[k-1][k-1][j]);
+                }
 
-    int* dp[V][V];
-
-    /*to k=0 (adjacent vertices)*/
-    for (int i = 0; i<V; i++){
-        for (int j = 0; j<V; j++){
-            dp[0][i][j] = graph[i][j];  // if vertices are not adjacent, distance remain <infinity> 
-        }
-    }
-    
-    /* the rest of permutations */
-    for (int k = 1; k<V; k++){
-        for (int i = 0; i<V; i++){
-            for (int j = 0; j<V; j++){
-                // dp[k] is computed above dp[k-1]
-                // at every dp[k] layer we test adding an adjancy to the path dp[k][i][j], but as we dont know exactly adjacencys (fringes) of dp[k-1][i] node, we test all dp[k-1][i][k]
-                // consider fringe the adjancencys of a non ramified path
-                dp[k][i][j] = min(  
-                    dp[k-1][i][j],
-                    dp[k-1][i][k] + dp[k-1][k][j]
-                );
-                
-                
+                else
+                {
+                    dp[k][i][j] = dp[k-1][i][j];
+                }
             }
-        }
-    }
+
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            graph[i][j] = dp[V][i][j];
 }
 
+int main() {
+    int graph[V][V] = {
+        {0,         INFINITY,   3,          INFINITY},
+        {2,         0,          INFINITY,   INFINITY},
+        {INFINITY,  7,          0,          1},
+        {6,         INFINITY,   INFINITY,   0}
+    };
 
-int main()
-{
+    floydwarshall(graph);
 
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (graph[i][j] == INFINITY)
+                cout << "INF ";
+            else
+                cout << graph[i][j] << ' ';
+        }
+        cout << '\n';
+    }
 
-
-
-
-
-	return 0;
+    return 0;
 }
