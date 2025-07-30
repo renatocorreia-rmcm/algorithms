c91e1789e7e6b4e96ff9444abf0dc676df73510b
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2025-07-30 13:03:34 -0300

    solved non-writing bug

--- sudoku.cpp
+++ sudoku.cpp
@@ -6,24 +6,17 @@ using namespace std;
 
 int grid[9][9];
 
-bool* get_possibilities(int r, int c){
-    bool* possibilities = new bool[9];
+vector<int>* get_possibilities(int r, int c){
+    bool* possibilities = new bool[10];
+    
     for (int i = 0; i < 9; i++) possibilities[i] = 1;
     
-    
+    /* remove from possibilities values already in grid subset */
     for (int i = 0; i < 9; i++)
     {
-        if (grid[r][i] != 0)  // check horizontal values
-        {
-            possibilities[grid[r][i]-1] = 0;
-        }        
-        if (grid[i][c] != 0)  // check vertical values
-        {
-            possibilities[grid[i][c]-1] = 0;
-        }        
-        
+        possibilities[grid[r][i]-1] = 0;      
+        possibilities[grid[i][c]-1] = 0;        
     }
-
     /* check square values */
     int row_margin = (r/3)*3;
     int col_margin = (c/3)*3;
@@ -31,29 +24,21 @@ bool* get_possibilities(int r, int c){
     {
         for (int j = 0; j < 3; j++)
         {
-            if (grid[i+row_margin][j+col_margin] != 0){
-
-                possibilities[grid[i+row_margin][j+col_margin]-1] = 0;
-                
-            }
+            possibilities[grid[i+row_margin][j+col_margin]-1] = 0;
         }
         
     }
 
-    return possibilities;
-
-}
-
-bool all_false(bool* possibilities){
-    for (int i = 0; i < 9; i++)
-    {
+    vector<int>* result = new vector<int>();
+    for (int i = 1; i <= 9; i++){
         if (possibilities[i])
         {
-            return false;
+            result->push_back(i);
         }
         
     }
-    return true;
+    return result;
+
 }
 
 bool sudoku(){
@@ -63,25 +48,18 @@ bool sudoku(){
         {
             if (grid[i][j]==0)
             {
-                bool* possibilities = get_possibilities(i,j);
-                if (all_false(possibilities))
+                vector<int>* possibilities = get_possibilities(i,j);
+                
+                if (possibilities->empty()) return false;
+                
+                for (int n: *possibilities)
                 {
-                    return false;
+                    grid[i][j] = n;
+                    if (sudoku()) return true;
+                    grid[i][j] = 0;                                 
                 }
-                
-                for (int k = 0; k < 9; k++)
-                {
-                    if (possibilities[k])
-                    {
-                        grid[i][j] = k+1;
-                        bool r = sudoku();
-                        grid[i][j] = 0;
-                        return r;                        
-                    }
-                    
-                }
-                
-                
+                delete possibilities;                
+                return false;
             }
             
         }      
@@ -90,6 +68,17 @@ bool sudoku(){
     
 }
 
+void print(){
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cout << grid[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+}
 
 
 int main(){
@@ -103,6 +92,7 @@ int main(){
     while (t--)
     {
         
+        /* INPUT */
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
@@ -112,19 +102,12 @@ int main(){
         }
         cout << '\n';
 
-        if (sudoku)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    cout << grid[i][j] << ' ';
-                }
-                cout << '\n';
-            }
-            cout << '\n';
-            
-        }
+        
+        /* SOLVE AND PRINT */
+        if (sudoku()) print();
+
+        else cout << "No solution" << '\n';
+        
         
 
     }
